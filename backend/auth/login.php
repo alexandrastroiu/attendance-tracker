a37edc422ff803e8422dbc960e4f8466fc4552e3
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-11-03 11:15:22 +0200

    Add login endpoint
    Add login endpoint: receives data, checks it against the database and
    returns a response.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ backend/auth/login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();  // Start the session
+
+require_once __DIR__ . '/../config/dbconnect.php';
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    
+    $password = $_POST['password'];
+
+
+    $stmt = $conn->prepare("SELECT * FROM Users WHERE username = :username");
+    $stmt->execute(['username' => $username]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && password_verify($password, $user['user_password'])) {
+        $_SESSION['user_id'] = $user['user_id'];
+        $_SESSION['username'] = $user['username'];
+
+        echo json_encode([
+            'success' => true,
+            'message' => 'Login successful'
+        ]);
+    }
+    else {
+        echo json_encode([
+            'success' => false,
+            'message' => 'Invalid username or password'
+        ]);
+
+    }
+}
+else {
+        echo json_encode([
+            'success' => false,
+            'message' => 'Invalid request'
+        ]);
+}
+
+?>
