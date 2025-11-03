2a03386ea542b210eb1b20c9bbc6de66f81bb69f
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-11-03 10:57:38 +0200

    Add connection test
    Test the PHP connection to the MySQL database.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ backend/test_db.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'config/dbconnect.php';
+
+// Test query
+try {
+    // Check the connection to the database
+    $stmt = $conn->query("SELECT DATABASE()"); 
+    $db = $stmt->fetchColumn();
+    echo "<br>Connection test was successful. Current DB: $db";
+} catch (PDOException $e) {
+    echo "Connection test failed: " . $e->getMessage();
+}
+?>
