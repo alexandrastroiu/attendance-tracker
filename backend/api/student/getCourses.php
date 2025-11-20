fb6d5cbccc157b3a5c1265feb4bcc839eca7d0bc
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-11-20 18:00:45 +0200

    Add php script to fetch all courses
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ backend/api/student/getCourses.php
@@ -0,0 +1,46 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
+
+require_once __DIR__ . '/../../config/dbconnect.php';
+session_start(); // Resume the session or start a new session
+
+try {
+
+    if (!isset($_SESSION['student_id'])) {
+        echo json_encode(["error" => "Not logged in"]);
+        exit;
+    }
+
+    //Get student ID (contained in the current session)
+    $student_id = intval($_SESSION['student_id']);
+
+    // SQL query that selects data for all courses in which the student is enrolled
+    $coursesQuery = $conn->prepare("
+    SELECT 
+    C.course_id, C.course_name, C.course_type, 
+    CE.enrollment_type,
+    CONCAT(T.first_name, ' ', T.last_name) AS teacher_name
+    FROM Course_Enrollment CE
+    JOIN Courses C ON CE.course_id = C.course_id
+    JOIN Teachers T ON T.teacher_id = C.teacher_id
+    WHERE CE.student_id = :student_id
+    ");
+
+    $coursesQuery->bindParam('student_id', $student_id, PDO::PARAM_INT);
+
+    $coursesQuery->execute();
+    //Fetch data
+    $studentCourses = $coursesQuery->fetchAll(PDO::FETCH_ASSOC);
+
+    //Encode data in JSON format
+    echo json_encode($studentCourses);
+
+}
+catch (Exception $e) {
+    // Error handling
+    echo json_encode(["error" => $e->getMessage()]);
+}
+
+?>
