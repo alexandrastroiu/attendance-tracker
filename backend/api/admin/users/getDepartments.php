fb3e1b8312a104d84358037a6682700cbcd77894
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-11-30 14:27:16 +0200

    Add get departments endpoint
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ backend/api/admin/users/getDepartments.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+require_once __DIR__ . '/../../../config/dbconnect.php';
+session_start();
+
+try {
+    $departments = $conn->prepare("
+    SELECT department_id, department_name
+    FROM Departments
+    ORDER By department_name ASC
+    ");
+    $departments->execute();
+    $dep = $departments->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($dep);
+}
+catch (Exception $e) {
+    echo json_encode(["error"=> $e->getMessage()]);
+}
+?>
