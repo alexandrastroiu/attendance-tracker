e6eac6788b8baa6b65066c393f2bcc6717828c68
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-12-01 00:39:13 +0200

    Add get teachers endpoint
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ backend/api/admin/courses/getTeachers.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+require_once __DIR__ . '/../../../config/dbconnect.php';
+session_start();
+
+try {
+    $teachers = $conn->prepare("
+    SELECT C.course_id, C.course_name, C.course_type, C.teacher_id,
+    CONCAT(T.first_name, ' ', T.last_name) AS teacher_name
+    FROM Courses C
+    LEFT JOIN Teachers T ON C.teacher_id = T.teacher_id
+    ");
+
+    $teachers->execute();
+
+    echo json_encode($teachers->fetchAll(PDO::FETCH_ASSOC));
+
+}
+catch (Exception $e) {
+    echo json_encode(["error"=> $e->getMessage()]);
+}
+
+?>
