242519e1eec01efed0bd2ed30290752df48bcd52
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-12-03 20:04:57 +0200

    Add main page
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ frontend/my-app/src/components/Mainpage/MainPage.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Main.css";
+import Picture from '../assets/flat-university-concept.png';
+
+export default function Main() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.body.classList.add("home-body");
+    return () => document.body.classList.remove("home-body");
+  }, []);
+
+  return (
+    <div className="home-wrapper">
+      <div className="home-card">
+
+        <img
+          src={Picture}
+          alt="university"
+          className="home-image"
+        />
+
+        <h2 className="home-dept-text">Department of Computer Science and Engineering</h2>
+
+        <h1 className="home-title">Attendance Management Website</h1>
+        <div className="home-underline"></div>
+
+        <p className="home-description">
+          A modern attendance platform that simplifies attendance tracking for both students and educators.
+        </p>
+
+        <button className="home-login-btn" onClick={() => navigate("/login")}>
+          Proceed to Login →
+        </button>
+      </div>
+    </div>
+  );
+}
